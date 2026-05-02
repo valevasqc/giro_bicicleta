28865623f57fc97cc7646ea84b311901fd8aaa49
Valeria <valeria@Valerias-MacBook-Air.local>
2026-05-01 20:31:30 -0600

    sensor debug

--- run_s1.sh
+++ run_s1.sh
@@ -6,5 +6,6 @@ export STATION_ID=S1
 export STATION_NAME="Estación 1"
 export STATION_HTTP_PORT=8001
 export FLASK_APP=app.py
+export STUB_SENSORS=false
 
 python3 -m flask run --host=127.0.0.1 --port=8001
