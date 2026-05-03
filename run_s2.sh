2cef61ed04af1ce93b3ffd0e48c1725886b17b6a
Valeria <valeria@Valerias-MacBook-Air.local>
2026-05-03 13:42:17 -0600

    s2 debug

--- run_s2.sh
+++ run_s2.sh
@@ -7,6 +7,5 @@ export STATION_NAME="Estación 2"
 export STATION_HTTP_PORT=8002
 export FLASK_APP=app.py
 export STUB_SENSORS=false
-export LOCK_UNLOCKS_WHEN_HIGH=false
 
 python3 -m flask run --host=127.0.0.1 --port=8002
