1669abe6f2662fff5d3aa53f4f6a0626d719a1b4
Valeria <valeria@Valerias-MacBook-Air.local>
2026-05-02 21:48:29 -0600

    debug station2

--- run_s2.sh
+++ run_s2.sh
@@ -5,6 +5,8 @@ cd "$(dirname "$0")/station"
 export STATION_ID=S2
 export STATION_NAME="Estación 2"
 export STATION_HTTP_PORT=8002
+export FLASK_APP=app.py
 export STUB_SENSORS=false
+export LOCK_UNLOCKS_WHEN_HIGH=false
 
-flask run --port 8002
+python3 -m flask run --host=127.0.0.1 --port=8002
